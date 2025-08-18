78a43a7d5823b523602c947768ef497338c09eaa
zipple <kaszipple@gmail.com>
2025-08-18 22:37:57 +0800

    sui除了core部分的修改

--- crates/typed-store-derive/src/lib.rs
+++ crates/typed-store-derive/src/lib.rs
@@ -351,6 +351,36 @@ pub fn derive_dbmap_utils_general(input: TokenStream) -> TokenStream {
                         #generics_names: #generics_bounds_token,
                     )*
                 > #intermediate_db_map_struct_name #generics {
+
+
+                /// Opens the tables in read-only mode but returns an instance of the original struct.
+                /// All write operations will fail at runtime.
+            #[allow(unused_parens)]
+            pub fn open_tables_read_only_as_rw_impl(
+                path: std::path::PathBuf,
+                metric_conf: typed_store::rocks::MetricConf,
+            ) -> Self {
+                let p: std::path::PathBuf = tempfile::tempdir()
+                    .expect("Failed to open temporary directory")
+                    .into_path();
+
+                let inner = Self::open_tables_impl(
+                    path,
+                    Some(p),
+                    false,
+                    metric_conf,
+                    None,
+                    None,
+                    false,
+                );
+                Self {
+                    #(
+                        #field_names: #post_process_fn(inner.#field_names),
+                    )*
+                }
+            }
+
+
                 /// Opens a set of tables in read-write mode
                 /// If as_secondary_with_path is set, the DB is opened in read only mode with the path specified
                 pub fn open_tables_impl(
@@ -537,6 +567,17 @@ pub fn derive_dbmap_utils_general(input: TokenStream) -> TokenStream {
                         )*
                     }
                 }
+                pub fn get_rw_handle_readonly_inner (
+                primary_path: std::path::PathBuf,
+                metric_conf: typed_store::rocks::MetricConf,
+            ) -> Self {
+                let inner = #intermediate_db_map_struct_name::open_tables_read_only_as_rw_impl(primary_path, metric_conf);
+                Self {
+                    #(
+                        #field_names: #post_process_fn(inner.#field_names),
+                    )*
+                }
+            }
 
                 /// This opens the DB in read only mode and returns a struct which exposes debug features
                 pub fn get_read_only_handle (
